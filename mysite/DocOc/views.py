6191300cbdbc9fe7e4926c9f27bd9e72b5bee620
sakay <sakay@github.com>
2019-02-16 22:30:13 -0800

    heatmap done

--- mysite/DocOc/views.py
+++ mysite/DocOc/views.py
@@ -47,67 +47,80 @@ def upload(request):
     results_viral = []
     results_demo = []    
     result = -1
-    try:
-        # if this is a POST request we need to process the form data
-        if request.method == 'POST' and request.FILES['myfile']:
-            myfile = request.FILES['myfile']
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
-            # create a form instance and populate it with data from the request:
-            
-            # Data preprocessing
-            demo_path = uploaded_file_url
-            mean_path = "DocOc/static/train_mean.csv"
-            std_path = "DocOc/static/train_std.csv"
-            genes_path = "DocOc/static/gene_order.csv"
+    flatmap_vals = []
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        # create a form instance and populate it with data from the request:
+        
+        # Data preprocessing
+        demo_path = uploaded_file_url
+        mean_path = "DocOc/static/train_mean.csv"
+        std_path = "DocOc/static/train_std.csv"
+        genes_path = "DocOc/static/gene_order.csv"
 
-            #demo should have first row be gene name and second row be value 
-            demo = np.loadtxt(demo_path, delimiter ="\t", dtype = str) #(2, 13321)
-            mean = np.loadtxt(mean_path, delimiter ="\t", dtype = float) #(100,)
-            std = np.loadtxt(std_path, delimiter ="\t", dtype = float) #(100,)
-            genes = np.loadtxt(genes_path, delimiter ="\t", dtype = str) #(100,)
+        #demo should have first row be gene name and second row be value 
+        demo = np.loadtxt(demo_path, delimiter ="\t", dtype = str) #(2, 13321)
+        mean = np.loadtxt(mean_path, delimiter ="\t", dtype = float) #(100,)
+        std = np.loadtxt(std_path, delimiter ="\t", dtype = float) #(100,)
+        genes = np.loadtxt(genes_path, delimiter ="\t", dtype = str) #(100,)
 
-            demo_dict = {}
-            for gene, value in demo.T:
-                demo_dict[gene.upper()] = float(value)
+        demo_dict = {}
+        for gene, value in demo.T:
+            demo_dict[gene.upper()] = float(value)
 
-            data = np.array([demo_dict[gene] if gene in demo_dict else mean[index] for index, gene in enumerate(genes)])
+        data = np.array([demo_dict[gene] if gene in demo_dict else mean[index] for index, gene in enumerate(genes)])
 
-            data = (data - mean) / std
-            data = np.expand_dims(data,axis=0)
+        data = (data - mean) / std
+        data = np.expand_dims(data,axis=0)
 
-            # loading the model
-            clf = load('DocOc/static/trained_svm.joblib')
+        # loading the model
+        clf = load('DocOc/static/trained_svm.joblib')
 
-            # predicting output
-            result = clf.predict(data)
+        # predicting output
+        result = clf.predict(data)
 
-            # scatter plot data stuff
-            file_name = "DocOc/static/train_scatter.csv"
-            data_X = np.genfromtxt(file_name, delimiter="\t", dtype = float, usecols = range(1, 101))
-            data_Y = np.genfromtxt(file_name, delimiter="\t", dtype = str, usecols = (0))
-            
-            data_X = np.concatenate((data_X, data), axis = 0)
-            data_Y = np.concatenate((data_Y, np.array(["2"])))
+        # scatter plot data stuff
+        file_name = "DocOc/static/train_scatter.csv"
+        data_X = np.genfromtxt(file_name, delimiter="\t", dtype = float, usecols = range(1, 101))
+        data_Y = np.genfromtxt(file_name, delimiter="\t", dtype = str, usecols = (0))
+        
+        data_X = np.concatenate((data_X, data), axis = 0)
+        data_Y = np.concatenate((data_Y, np.array(["2"])))
+        np.random.seed(1)
+        pca = decomposition.PCA(n_components = 50)
+        results = pca.fit_transform(data_X)
+        tsne = manifold.TSNE(verbose = 1)
+        results = tsne.fit_transform(results)
 
-            pca = decomposition.PCA(n_components = 50)
-            results = pca.fit_transform(data_X)
-            tsne = manifold.TSNE(verbose = 1)
-            results = tsne.fit_transform(results)
+        for index, value in enumerate(data_Y):
+            if value == "0":
+                results_bacteria.append(results[index].tolist())
+            elif value == "1":
+                results_viral.append(results[index].tolist())
+            else:
+                results_demo.append(results[index].tolist())
+        
+        # heat map stuff
+        indices_order_cols = np.loadtxt("DocOc/static/heatmap_sort_indices", delimiter = "\t", dtype = int)
+        train_sorted = np.loadtxt("DocOc/static/heatmap_train.csv", delimiter = "\t", dtype = float)
 
-            for index, value in enumerate(data_Y):
-                if value == "0":
-                    results_bacteria.append(results[index].tolist())
-                elif value == "1":
-                    results_viral.append(results[index].tolist())
-                else:
-                    results_demo.append(results[index].tolist())
+        data = [data[0][index] for index in indices_order_cols]        
+        data = np.expand_dims(data, axis = 0)
+        _sorted = np.concatenate((train_sorted[:5],train_sorted[-5:]),axis=0)
 
-    except:
-        result = "Oops! Something went wrong!"
+        heatmap_values = np.concatenate((_sorted, data), axis = 0).T
 
-    return render(request, 'DocOc/verdict.html',{'result': result,'x_scatter_data': results_bacteria,'y_scatter_data':results_viral,'z_scatter_data':results_demo})
+        
+        for index,row in enumerate(heatmap_values):
+            for i2, col in enumerate(row):
+                flatmap_vals.append([index,i2,col])
+
+
+    return render(request, 'DocOc/verdict.html',{'result': result,'x_scatter_data': results_bacteria,'y_scatter_data':results_viral,'z_scatter_data':results_demo,'heatmap_data':flatmap_vals})
 
 
 @csrf_exempt
